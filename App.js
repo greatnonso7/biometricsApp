91fb1fab93be4eee228c20a59eeec9f8190c6c5f
Ichoku Chinonso <greatchinonso7@gmail.com>
2021-06-02 20:20:10 +0100

    Face ID and touch Id working for android and Ios

--- App.js
+++ App.js
@@ -1,112 +1,44 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
+import {Alert, Text, TouchableOpacity, Button, View} from 'react-native';
+import TouchID from 'react-native-touch-id';
+const optionalConfigObject = {
+  title: 'Authentication Required', // Android
+  color: 'ffffff', // Android,
+  fallbackLabel: 'Show Passcode', // iOS (if empty, then label is hidden)
 };
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+class App extends React.Component {
+  touchIdAuth = () => {
+    TouchID.isSupported()
+      .then(biometryType => {
+        // Success code
+        if (biometryType === 'FaceID') {
+          console.log('FaceID is supported.');
+        } else {
+          console.log('TouchID is supported.');
+          TouchID.authenticate('Authenticate', optionalConfigObject)
+            .then(success => {
+              Alert.alert('Authenticated Successfully');
+            })
+            .catch(error => {
+              Alert.alert('Authentication Failed', error.toString());
+            });
+        }
+      })
+      .catch(error => {
+        // Failure code
+        console.log(error);
+      });
   };
 
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
+  render() {
+    return (
+      <View style={{justifyContent: 'center', flex: 1, alignSelf: 'center'}}>
+        <TouchableOpacity style={{flexWrap: 'wrap'}}>
+          <Button title="Authenticate" onPress={this.touchIdAuth.bind(this)} />
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
 export default App;
